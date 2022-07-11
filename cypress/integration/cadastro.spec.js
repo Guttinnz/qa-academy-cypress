a13dee346999c3f24669fe6aaec0d4b3693fa539
Gustavo Bueno <gustavoriedel2202@gmail.com>
2022-07-11 16:51:01 -0300

    New ITs On The Block (Step by step)

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -57,15 +57,39 @@ describe('Cadastro', () => {
         signupPage.alertMessageShouldBe('Oops! Email com formato inválido.')
     })
 
-    it('Required fields', function(){
-        signupPage.go()
-        signupPage.submit()
-        signupPage.alertMessageShouldBe('É necessário informar o nome')
-        signupPage.alertMessageShouldBe('É necessário informar o CPF')
-        signupPage.alertMessageShouldBe('É necessário informar o email')
-        signupPage.alertMessageShouldBe('É necessário informar o CEP')
-        signupPage.alertMessageShouldBe('É necessário informar o número do endereço')
-        signupPage.alertMessageShouldBe('Selecione o método de entrega')
-        signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+    context('Required fields', function () {
+
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery_method', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' },
+        ]
+
+        before(function () { // funções para fazer teste de ação
+            signupPage.go()
+            signupPage.submit()
+        })
+
+        messages.forEach(function (msg) { // substitui os testes it, testes dinâmicos onde se um falhar não interrompe o teste completo
+            it(`${msg.field} is required`, function () {
+                signupPage.alertMessageShouldBe(msg.output)
+            })
+        })
     })
+
+    // it('Required fields', function () {
+    //     signupPage.go()
+    //     signupPage.submit()
+    //     signupPage.alertMessageShouldBe('É necessário informar o nome')
+    //     signupPage.alertMessageShouldBe('É necessário informar o CPF')
+    //     signupPage.alertMessageShouldBe('É necessário informar o email')
+    //     signupPage.alertMessageShouldBe('É necessário informar o CEP')
+    //     signupPage.alertMessageShouldBe('É necessário informar o número do endereço')
+    //     signupPage.alertMessageShouldBe('Selecione o método de entrega')
+    //     signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+    // })
 })
